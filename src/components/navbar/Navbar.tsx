1f9a6488ce87ca1158158a488fd80491b5c48563
Rodrigo-Santos-Dias <digoh.a4@gmail.com>
2024-08-22 02:32:48 -0300

    Remoção dos componentes navbar/footer no cadastro e login

--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -1,5 +1,5 @@
 import { Link, useNavigate } from 'react-router-dom'
-import React,{ useContext } from 'react'
+import { useContext } from 'react'
 import { AuthContext } from '../../contexts/AuthContext'
 
 function Navbar() {
@@ -16,24 +16,30 @@ function Navbar() {
   
   let navbarComponent
 
+  if(user.token !== "") {
+    navbarComponent = (
+      <div className='w-full bg-gray-800 text-white flex justify-center py-4'>
+      <div className="container flex justify-between text-lg">
+        <div className='text-2xl font-bold uppercase'>Luminar</div>
+
+        <div className='flex gap-4'>
+          <Link to='/login' className='hover:underline'>Login</Link>
+          <Link to='/home' className='hover:underline'>Home</Link>
+          <Link to="/product" className="hover:underline">Products</Link>
+          <Link to='/category' className='hover:underline'>Categories</Link>
+          <Link to="/registerCategory"className='hover:underline'>Register Category</Link>
+          <Link to="/registerProduct"className='hover:underline'>Register Product</Link>
+          <div className='hover:underline'>Profile</div>
+          <Link to='' onClick={logout} className='hover:underline'>Logout</Link>
+        </div>
+      </div>
+    </div>
+    )
+  }
+
   return (
     <>
-     <div className='w-full bg-gray-800 text-white flex justify-center py-4'>
-          <div className="container flex justify-between text-lg">
-            <div className='text-2xl font-bold uppercase'>Luminar</div>
-
-            <div className='flex gap-4'>
-              <Link to='/login' className='hover:underline'>Login</Link>
-              <Link to='/home' className='hover:underline'>Home</Link>
-              <Link to="/product" className="hover:underline">Products</Link>
-              <Link to='/category' className='hover:underline'>Categories</Link>
-              <Link to="/registerCategory"className='hover:underline'>Register Category</Link>
-              <Link to="/registerProduct"className='hover:underline'>Register Product</Link>
-              <div className='hover:underline'>Profile</div>
-              <Link to='' onClick={logout} className='hover:underline'>Logout</Link>
-            </div>
-          </div>
-        </div>
+       {navbarComponent}
     </>
   )
 }
