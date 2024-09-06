5433fd58b7a2766b4c18d3b5682b1dbbe2557245
Rodrigo-Santos-Dias <digoh.a4@gmail.com>
2024-09-06 11:03:35 -0300

    modificando footer

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -10,16 +10,16 @@ function Footer() {
   if(user.token !== "") {
     footerComponent = (
       <div className="flex justify-center bg-indigo-900 text-white">
-        <div className="container flex flex-col items-center py-4">
-          <img src={logo} alt="Luminar logo" className="h-21" /> {/* Adiciona a logo */}
-          <div className='flex gap-6'>
-            <InstagramLogo size={32} weight='bold' />
-            <FacebookLogo size={32} weight='bold' />
-            <YoutubeLogo size={32} weight='bold' />
-            <WhatsappLogo size={32} weight='bold' />
-          </div>
+      <div className="container flex flex-col items-center py-6">
+        <img src={logo} alt="Luminar logo" className="h-20 mb-4" /> {/* Ajusta a altura da logo */}
+        <div className="flex gap-6">
+          <InstagramLogo size={32} weight="bold" />
+          <FacebookLogo size={32} weight="bold" />
+          <YoutubeLogo size={32} weight="bold" />
+          <WhatsappLogo size={32} weight="bold" />
         </div>
-      </div>    
+      </div>
+    </div>
   )}
 
     return (
